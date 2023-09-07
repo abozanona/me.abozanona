df19a4923974f13616c8041cf2d3a864d1d61464
abozanona <abozanona@gmail.com>
2023-09-07 17:03:32 +0200

    Fix fetching ty video

--- themes/sagiri/source/assets/yt/index.php
+++ themes/sagiri/source/assets/yt/index.php
@@ -1,9 +1,23 @@
 <?php
 $id = $_GET['id'];
 
-$res = file_get_contents("https://yt2html5.com/?id=$id");
+$opts = [
+    "http" => [
+        "method" => "GET",
+        "header" => "Content-Type: application/x-www-form-urlencoded; charset=UTF-8\r\n" .
+            "Referer: https://en1.y2mate.is/\r\n"
+    ]
+];
+$context = stream_context_create($opts);
+
+$res = file_get_contents("https://srvcdn15.2convert.me/api/json?url=https://www.youtube.com/watch?v=$id", false, $context);
 $res = json_decode($res);
-
-$url = $res->data->player_response->streamingData->formats[0]->url;
-
+$videos = $res->formats->video;
+foreach($videos as $video) {
+    if(!$video->needConvert) {
+        header('location:' . $video->url);
+        die('');
+    }
+}
+$url = $res->formats->video[0]->url;
 header('location:' . $url);
